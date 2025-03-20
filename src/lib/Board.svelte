edf9230d41e6c824900a9389d832b53ce12dcd76
AndreCruz2005 <avnc@cin.ufpe.br>
2025-03-20 00:57:55 -0300

    Refactoring

--- src/lib/Board.svelte
+++ src/lib/Board.svelte
@@ -5,17 +5,62 @@
     import { getContext, onMount } from "svelte";
     import { hideNumbers } from "../global/hideNumbers";
 
+    class BoardCell {
+        default = 0;
+        inserted = 0;
+        valid = true;
+        boxI = 0;
+        cellI = 0;
+        colI = 0;
+        rowI = 0;
+        focused = false;
+
+        /**
+         * @param {number} i
+         * @param {number} j
+         * @param {number} val
+         */
+        constructor(i, j, val) {
+            this.default = val;
+
+            this.boxI = i;
+            this.subBoxI = j;
+
+            this.getRow();
+            this.getColumn();
+        }
+
+        getRow() {
+            // Each box is 3×3
+            // First find which row the box is in (0, 1, or 2)
+            const boxRow = Math.floor(this.boxI / 3);
+
+            // Then find which row within the box the cell is in (0, 1, or 2)
+            const subBoxRow = Math.floor(this.subBoxI / 3);
+
+            // Combine to get the actual row (0-8)
+            this.rowI = boxRow * 3 + subBoxRow;
+        }
+
+        getColumn() {
+            // Each box is 3×3
+            // First find which column the box is in (0, 1, or 2)
+            const boxCol = this.boxI % 3;
+
+            // Then find which column within the box the cell is in (0, 1, or 2)
+            const subBoxCol = this.subBoxI % 3;
+
+            // Combine to get the actual column (0-8)
+            this.colI = boxCol * 3 + subBoxCol;
+        }
+    }
+
     // Constants
     const backend = getContext("backend");
 
     // Board state
     let solution = [];
     let sudokuBoard = $state([]);
-
-    // Relevant cells
-    let focusedCell = $state([]);
-    let relatedCells = $state({}); // In same box, row and col as focused cell
-    let occupiedCells = $state({}); // Where a number has been inserted
 
     // Difficulty settings
     const difficulties = { EASY: 36, MEDIUM: 29, HARD: 22 };
@@ -25,9 +70,17 @@
         axios
             .get(`${backend}/sudoku?hints=81&boxes=true`)
             .then((res) => {
-                for (let i = 0; i < res.data.length; i++)
-                    solution[i] = res.data[i].slice();
-                sudokuBoard = hideNumbers(res.data, difficulties[difficulty]);
+                for (let i = 0; i < res.data.length; i++) solution[i] = res.data[i].slice();
+
+                // Create sudoku board
+                const boardArr = hideNumbers(res.data, difficulties[difficulty]);
+                let boardObjects = [];
+                for (let i = 0; i < 9; i++) {
+                    for (let j = 0; j < 9; j++) {
+                        boardObjects.push(new BoardCell(i, j, boardArr[i][j]));
+                    }
+                }
+                sudokuBoard = boardObjects;
             })
             .catch((err) => {
                 console.error(err);
@@ -35,158 +88,45 @@
     };
 
     const checkWin = () => {
-        for(const box in solution){
-            for (const cell in box){
-                let i = sudokuBoard.indexOf(box), j = box.indexOf(cell);
-                if (cell == sudokuBoard[i][j] || cell == eval(occupiedCells[`${i},${j}`])) {
-                    continue;
-                }
-                else{
-                    console.log('unsolved');
-                    return;
-                }
-            }
-        }
-        console.log('solved!');
-    }
+        console.log("fuck you");
+    };
 
     onMount(() => {
         getSudoku();
     });
 
-    // When selecting a cell, gets row, column and box of the cell
-    function getRelatedCells(boxI, cellI) {
-        let cells = {};
+    $effect(() => {
+        console.log(sudokuBoard);
+    });
 
-        // Get all cells within the same box
-        for (let i = 0; i < 9; i++) {
-            cells[`${boxI},${i}`] = sudokuBoard[boxI][i];
-        }
-
-        // Convert boxI and cellI to global row and column
-        // Each box is 3x3
-        // boxI: 0 1 2
-        //       3 4 5
-        //       6 7 8
-        const boxRow = Math.floor(boxI / 3);
-        const boxCol = boxI % 3;
-
-        // cellI: 0 1 2
-        //        3 4 5
-        //        6 7 8
-        const cellRow = Math.floor(cellI / 3);
-        const cellCol = cellI % 3;
-
-        // Get all cells in the same row
-        for (let boxColIndex = 0; boxColIndex < 3; boxColIndex++) {
-            const rowBoxI = boxRow * 3 + boxColIndex;
-
-            for (let cellColIndex = 0; cellColIndex < 3; cellColIndex++) {
-                const rowCellI = cellRow * 3 + cellColIndex;
-                const cellKey = `${rowBoxI},${rowCellI}`;
-
-                cells[cellKey] = sudokuBoard[rowBoxI][rowCellI];
-                
-            }
-        }
-
-        // Get all cells in the same column
-        for (let boxRowIndex = 0; boxRowIndex < 3; boxRowIndex++) {
-            const colBoxI = boxRowIndex * 3 + boxCol;
-            
-            for (let cellRowIndex = 0; cellRowIndex < 3; cellRowIndex++) {
-                const colCellI = cellRowIndex * 3 + cellCol;
-                const cellKey = `${colBoxI},${colCellI}`;
-
-                cells[cellKey] = sudokuBoard[colBoxI][colCellI];
-                
-            }
-        }
-
-        relatedCells = cells;
-    }
-
-    // Display either the cell's default value or a value inserted by the user
     /**
-     * @param {string} cell
-     * @param {number} boxI
-     * @param {number} cellI
+     * @param {BoardCell} cell
      */
-    function getCellValue(cell, boxI, cellI) {
-        const key = `${[boxI, cellI]}`;
-
-        if (key in occupiedCells) return occupiedCells[key];
-        return cell != "0" ? cell : "";
-    }
-
-    function findConflicts(cellKey){
-        if(relatedCells[cellKey] == occupiedCells[`${focusedCell[0]},${focusedCell[1]}`]) return "invalid";
-        return null;
-    }
-
-    function getCellClass(cell, boxI, cellI) {
-        const cellKey = `${[boxI, cellI]}`;
-
-        if (focusedCell[0] == boxI && focusedCell[1] == cellI) {
-            return "active";
-        }
-        if (cellKey in relatedCells) {
-            const isInvalid = findConflicts(cellKey);
-            return isInvalid || "related";
-        }
-        if (cellKey in occupiedCells) {
-            return "occupied";
-        }
-        return "cell";
+    function getCellValue(cell) {
+        const returnValue = cell.inserted ? cell.inserted : cell.default;
+        return returnValue ? returnValue : "";
     }
 </script>
 
 <div id="game">
     <div id="board">
-        {#each sudokuBoard as box, boxI}
+        {#each Array.from({ length: 9 }, (_, i) => i) as boxI}
             <div class="box">
-                {#each box as cell, cellI}
-                    <div
-                        class={getCellClass(cell, boxI, cellI)}
-                        onclick={() => {
-                            focusedCell = focusedCell[0] === boxI && focusedCell[1] === cellI ? [] : [boxI, cellI];
-                            if(focusedCell.length > 0) getRelatedCells(focusedCell[0], focusedCell[1]);
-                            else relatedCells = {};
-                        }}
-                    style={`${[boxI, cellI]}` in occupiedCells ? "color: rgb(221, 186, 116);" : ""}>
-                        {getCellValue(cell, boxI, cellI)}
-                    </div>
+                {#each sudokuBoard.filter((i) => i.boxI == boxI) as cell}
+                    <div>{getCellValue(cell)}</div>
                 {/each}
             </div>
         {/each}
     </div>
     <div id="numpad">
         {#each Array.from({ length: 9 }, (_, i) => i + 1) as num}
-            <button
-                onclick={() => {
-                    if (focusedCell && sudokuBoard[focusedCell[0]][focusedCell[1]] === 0) {
-                        occupiedCells[`${focusedCell}`] = num;
-                        checkWin();
-                    }
-                }}
-                class="numbutton">{num}</button
-            >
+            <button class="numbutton">{num}</button>
         {/each}
-        <button
-            onclick={() => {
-                delete occupiedCells[`${focusedCell}`];
-            }}>CLEAR</button
-        >
-        <button  onclick={() => {
-            let newB = [...sudokuBoard];
-            newB[focusedCell[0]][focusedCell[1]] = solution[focusedCell[0]][focusedCell[1]];
-            sudokuBoard = newB;
-        }}>HINT</button>
+        <button>CLEAR</button>
+        <button>HINT</button>
         <button
             onclick={() => {
                 getSudoku();
-                focusedCell = [];
-                occupiedCells = [];
             }}>NEW</button
         >
     </div>
@@ -236,9 +176,9 @@
         background-color: light-dark(rgb(143, 221, 236), #6c7e83);
     }
     .box div.related {
-       background-color: light-dark(aquamarine, #4d585c);
+        background-color: light-dark(aquamarine, #4d585c);
     }
-    .box div.invalid{
+    .box div.invalid {
         background-color: rgb(196, 122, 122);
     }
 </style>
